303ac9dbc259f9babf773ad4b1a4e70d34e256ee
Mori <jardenlee@163.com>
2023-03-06 14:13:01 +0800

    Initial Commit

---
+++ ProjectGT2.1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ProjectGT2.1
+//
+//  Created by MORI on 2023/3/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProjectGT2.1/ProjectGT2_1App.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectGT2_1App.swift
+//  ProjectGT2.1
+//
+//  Created by MORI on 2023/3/6.
+//
+
+import SwiftUI
+
+@main
+struct ProjectGT2_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
